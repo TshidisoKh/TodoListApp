630290b20d6beb8a883a81981987c18bb70608fb
Tshidiso <tshidiso.khoza@standardbank.co.za>
2023-02-28 06:47:49 +0200

    initial commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by Tshidiso Khoza on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
