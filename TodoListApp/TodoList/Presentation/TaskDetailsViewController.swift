5c43b87b507a4bccc44c80dee1baeaf16ef9753c
Tshidiso <tshidiso.khoza@standardbank.co.za>
2023-03-10 13:26:39 +0200

    fix service call issue with calling the data

---
+++ TodoListApp/TodoList/Presentation/TaskDetailsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TaskDetailsViewController.swift
+//  TodoListApp
+//
+//  Created by Tshidiso Khoza on 2023/03/10.
+//
+
+import Foundation
---
+++ TodoListApp/TodoList/ViewControllers/CustomTaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTaskTableViewCell.swift
+//  TodoListApp
+//
+//  Created by Tshidiso Khoza on 2023/03/10.
+//
+
+import UIKit
+
+class CustomTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
